14d405090505fce46d10238f79e393db5989dd35
OzHache <OzHache@gmail.com>
2020-02-21 17:08:52 -0800

    Week 3 Update 1
    Boat Physics converted to Nav Mesh Agent. Import of Boat prefab/shaders and Textures

---
+++ River Styx/Assets/Scripts/BoatTravel.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BoatTravel : MonoBehaviour
+{
+    public Waypoint currentWaypoint;
+    private Transform target { get { return currentWaypoint.transform; }}
+    [SerializeField] NavMeshAgent navMeshAgent;
+    [SerializeField] WaypointManager waypointManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(currentWaypoint == null)
+        {
+            currentWaypoint = waypointManager.startingWaypoint;
+        }
+        navMeshAgent.SetDestination(target.position);
+        if (navMeshAgent.remainingDistance >= navMeshAgent.stoppingDistance)
+        {
+            GetNextWaypoint();
+        }
+    }
+
+    void GetNextWaypoint()
+    {
+        currentWaypoint = waypointManager.NextWaypoint(currentWaypoint);
+        
+    }
+}
